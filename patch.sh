d0c70e453fbfc44394cc37c81ac3f06eb0cf32e1
Dzianis Nikifaraŭ <wirgen@users.noreply.github.com>
2023-10-31 19:50:39 +0300

    Create patch.sh

---
+++ patch.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+declare -a mirror_list=(
+    "packages.microsoft.com/clamav"
+    "database.clamav.net"
+)
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit 1
+fi
+
+echo "Available mirrors:"
+PS3="Choose a mirror:"
+select option in "${mirror_list[@]}" "Quit"
+do
+    if [[ $REPLY = "$(( ${#mirror_list[@]}+1 ))" ]]; then
+        echo "Goodbye"
+        exit 0
+    fi
+
+    mirror="$option"
+    break
+done
+
+sed -i "s|DatabaseMirror .*|DatabaseMirror $mirror|" /var/packages/AntiVirus/target/engine/clamav/etc/freshclam.conf
+
+echo "Update clamav config successfully."
