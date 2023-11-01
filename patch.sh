a8358b1eafb9752fbf7360ea80320d2abb479354
Dzianis Nikifaraŭ <wirgen@users.noreply.github.com>
2023-11-01 14:58:26 +0300

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -10,16 +10,45 @@ if (( $EUID != 0 )); then
     exit 1
 fi
 
-echo "Available mirrors:"
-PS3="Choose a mirror:"
-select option in "${mirror_list[@]}" "Quit"
+declare -a available=()
+declare -a result=()
+
+echo "Mirrors testing:"
+
+for i in "${!mirror_list[@]}"; do
+    echo -n "$((i+1))/${#mirror_list[@]} ${mirror_list[$i]}... "
+    response=$(curl --silent --range 0-511 --write-out "%{http_code}" --output daily.cvd.part "https://${mirror_list[$i]}/daily.cvd")
+
+    if [ $response -lt 300 ] ; then
+        name=$(cut -d ":" -f1 daily.cvd.part)
+        dt=$(cut -d ":" -f2 daily.cvd.part)
+
+        if [ "$name" = "ClamAV-VDB" ]; then
+            echo "Success"
+            result+=("${mirror_list[$i]} > $dt")
+            available+=("${mirror_list[$i]}")
+        else
+            echo "Failed (Wrong file)"
+        fi
+    else
+        echo "Failed (Error $response)"
+    fi
+
+    rm daily.cvd.part 2> /dev/null
+done
+
+echo -e "\nAvailable mirrors:"
+
+PS3="Choose a mirror: "
+COLUMNS=1
+select option in "${result[@]}" "Quit"
 do
-    if [[ $REPLY = "$(( ${#mirror_list[@]}+1 ))" ]]; then
+    if [[ $REPLY = "$(( ${#result[@]}+1 ))" ]]; then
         echo "Goodbye"
         exit 0
     fi
 
-    mirror="$option"
+    mirror="${available[${REPLY}-1]}"
     break
 done
 
